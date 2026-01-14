85572ba1455990e1c2e6df488290325ee41fc383
Aregus <74340447+Areg472@users.noreply.github.com>
2026-01-14 18:06:00 +0400

    Update Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,11 +2,15 @@
 defineProps({
     href: String,
     class: String,
+    target: {
+        type: String,
+        default: "_blank",
+    },
 });
 </script>
 
 <template>
-    <a :href="href">
+    <a :href="href" :target="target">
         <button
             :class="$props.class"
             class="bg-gray-200 cursor-pointer border-black border-2 text-black rounded-xl w-40 h-10"
